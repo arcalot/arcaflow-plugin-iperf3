79834c3c6af5abb362135ab96d73a9f22eee9e6a
Dustin Black <dblack@redhat.com>
2023-07-10 20:33:34 +0200

    iterate on tests

--- tests/test_arcaflow_plugin_iperf3.py
+++ tests/test_arcaflow_plugin_iperf3.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
+
 import unittest
 import iperf3_plugin
+import iperf3_schema
+from multiprocessing.pool import ThreadPool
 from arcaflow_plugin_sdk import plugin
+import pprint
 
 
-class HelloWorldTest(unittest.TestCase):
+def run_iperf3_server():
+    server_input = iperf3_schema.ServerAllParams(
+        run_duration=10,
+        port=50000,
+        interval=1,
+        forceflush=True,
+    )
+    return iperf3_plugin.iperf3_server(server_input)
+
+class iperf3Test(unittest.TestCase):
     @staticmethod
     def test_serialization():
         plugin.test_object_serialization(
@@ -16,34 +29,45 @@ class HelloWorldTest(unittest.TestCase):
                 bytes=100,
                 # FIXME -- Apparent bug in test_object_serializtion not
                 # accepting a string value when using units
-                # bitrate='100K',
-                # FIXME -- Apparent bug as above but with enums
-                # format='g',
+                bitrate=100000,
+                format=iperf3_schema.Format.g,
             )
         )
 
-        # TODO -- Add output serialization test
-        # plugin.test_object_serialization(
-        #     iperf3_plugin.SuccessOutput(
-
-        #     )
-        # )
+        # TODO -- Add client output serialization test
+        plugin.test_object_serialization(
+            iperf3_plugin.ServerSuccessOutput(
+                message="message",
+            )
+        )
 
         plugin.test_object_serialization(
             iperf3_plugin.ClientErrorOutput(error="This is an error")
         )
 
-    # TODO -- Add functional tests
-    # def test_functional(self):
-    #     input = iperf3_plugin.InputParams(name="Example Joe")
+    def test_functional(self):
+        pool = ThreadPool(processes=1)
 
-    #     output_id, output_data = iperf3_plugin.hello_world(input)
+        pp = pprint.PrettyPrinter(indent=4)
+        
+        iperf3_server = pool.apply_async(run_iperf3_server)
 
-    #     # The example plugin always returns an error:
-    #     self.assertEqual("success", output_id)
-    #     self.assertEqual(
-    #         output_data, iperf3_plugin.SuccessOutput("Hello, Example Joe!")
-    #     )
+        client_input = iperf3_schema.ClientInputParams(
+            port=50000,
+            interval=10,
+            time=5,
+        )
+
+        client_output_id, client_output_data = iperf3_plugin.iperf3_client(client_input)
+        pp.pprint(client_output_data.output.start)
+
+        self.assertEqual("success", client_output_id)
+        # self.assertEqual("TCP", client_output_data.output.start.test_start.protocol)
+        
+        server_output_id, server_output_data = iperf3_server.get()
+
+        self.assertEqual("success", server_output_id)
+        self.assertEqual("message", server_output_data.message)
 
 
 if __name__ == "__main__":
